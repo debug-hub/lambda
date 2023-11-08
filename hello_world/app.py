892658a34cd97321b568093567319568f72d9d7a
sharyansh <sharymalik3363@gmail.com>
2023-11-08 12:53:14 +0530

    update2 commit

--- hello_world/app.py
+++ hello_world/app.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "helloworld",
+            "message": "hello",
             "location": ip.text.replace("\n", "")
         }),
     }
